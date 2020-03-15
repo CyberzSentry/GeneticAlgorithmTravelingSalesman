db40f4319a7be1296448cd3d6609dfdba8e15ed0
Maksym Brzęczek <maksym.brzeczek@gmail.com>
2020-03-15 22:40:13 +0100

    update

--- main.R
+++ main.R
@@ -6,21 +6,52 @@ n=0.8
 pm=0.2
 Tmax=1000
 
-calculateDistance <- function(citya, cityb){
+#Function calculating distance from one city to another
+calculateCityToCity <- function(citya, cityb){
   a = abs(citya[1] - cityb[1])
   b = abs(citya[2] - cityb[2])
   return(sqrt((a^2)+(b^2)))
 }
 
-initialPopulation <- function(cities, pSize){
-  outData <- list()
-  for(i in 1:pSize){
-    outData[[i]] <- cities[,sample(1:10)]
+#Function creating a distance matrix for all the cities
+calculateDistanceMatrix <- function(cities){
+  output <- matrix(nrow=length(cities[1,]), ncol = length(cities[1,]))
+  for(i in 1:length(cities[1,])){
+    for(j in 1:length(cities[1,])){
+      output[i,j] = calculateCityToCity(cities[,i], cities[,j])
+    }
   }
+  return(output)
+}
+
+#Function that creates the initial population
+initialPopulation <- function(cities, pSize){
+  outData <- replicate(pSize, sample(1:length(cities[1,]), replace = FALSE))
+  print(outData)
   return(outData)
 }
 
-dat = matrix(c(datx, daty), nrow=2, ncol = 10, byrow = TRUE)
-print(dat)
-print(calculateDistance(dat[,1], dat[,2]))
+populationEvaluation <- function(population, distansMatrix){
+  output <- numeric(length(population[1,]))
+  for(i in 1:length(output)){
+    output[i] <- distansMatrix[population[1,i], population[-1,i]]
+    for(j in 1:(length(population[,1])-1)){
+      output[i] <- output[i] + distansMatrix[population[j,i],population[j+1,i]]
+    }
+  }
+  print(output)
+}
 
+cities = matrix(c(datx, daty), nrow=2, ncol = 10, byrow = TRUE)
+print("Initial city coordinates:")
+print(dat)
+
+distanceMatrix <- calculateDistanceMatrix(dat)
+print("Distance matrix:")
+print(distanceMatrix)
+
+population <- initialPopulation(cities, P)
+print("Initial population: ")
+head(population)
+
+populationEvaluation(population, distanceMatrix)
